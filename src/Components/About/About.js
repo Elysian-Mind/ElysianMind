861e1a003c55dd3e961c5d574243a365f8c17c3f
Yvonne Mbuvi <mwendembuvi10@gmail.com>
2024-07-21 02:07:02 +0300

    Revert "Merge Files"

---
+++ src/Components/About/About.js
@@ -0,0 +1,38 @@
+import React from 'react';
+import about from '../Assets/about-us-image.jpg'
+import '../../Components/About/About.css'
+
+
+  
+
+function About() {
+  return (
+    <div className='about-us' id='about'>
+        <div className="row">
+            <div className="column">
+                <div className="about-text">
+                    <h1>About Us</h1>
+                    <p>We connect you with licensed therapists for confidential 
+                        online therapy sessions via video, phone, or text.
+                        Our platform uses data to recommend therapists suited to your needs and 
+                        offers personalized guidance. 
+                        You can also connect with others or vent anonymously in our secure online space. 
+                        We work with most insurance providers 
+                        to ensure affordable access to mental healthcare for everyone.</p>
+                        <button> Sign up</button>
+
+                </div>
+
+            </div>
+
+            <div className="column">
+                <img src={about} alt="" />
+            </div>
+        </div>
+
+        
+    </div>
+  )
+}
+
+export default About
---
+++ src/Components/HowItWorks/HowItWorks.js
@@ -0,0 +1,49 @@
+import React from 'react'
+import '../../Components/HowItWorks/HowItWorks.css'
+import textsms from '../Assets/text.png'
+import therapy from '../Assets/therapy.png'
+import plan from '../Assets/Plan.png'
+import breakout from '../Assets/breakoutroom.png'
+
+function HowItWorks() {
+  return (
+    <div className='howitworks' id='howitworks'>
+        <h1>How it Works</h1>
+        {/* <p>Here are some few steps to help you out with the web application</p> */}
+        <div className="how-it-works">
+            <div className="card">
+                <img src={textsms} alt="" />
+                <div className="container">
+                    <h4>CHAT</h4>
+                    <p>Get to tlak to our AI trained doctor!</p>
+                </div>
+            </div>
+            <div className="card">
+                <img src={therapy} alt="" />
+                <div className="container">
+                    <h4>THERAPY SESSION</h4>
+                    <p>Book therapy session with our therapists!</p>
+                </div>
+            </div>
+            <div className="card">
+                <img src={plan} alt="" />
+                <div className="container">
+                    <h4>PLAN</h4>
+                    <p>Create a suitable plan for your therapy sessions and activities!</p>
+                </div>
+            </div>
+            <div className="card">
+                <img src={breakout} alt="" />
+                <div className="container">
+                    <h4>VENT</h4>
+                    <p>Use our breakout room to vent anonymously!</p>
+                </div>
+            </div>
+        </div>    
+        <button>Join Us</button>
+
+    </div>
+  )
+}
+
+export default HowItWorks
