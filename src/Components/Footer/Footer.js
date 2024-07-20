2459068cdde5c6e4c6711f20ec8405170af4d992
MbuviM <mwendembuvi10@gmail.com>
2024-07-21 01:58:33 +0300

    Revert "Merge pull request #6 from Elysian-Mind/master" Revert "Merge pull request" This reverts commit 769c1106579915f7ecc50d6b02c5a9f2165abf39, reversing changes made to e98a25992cca77d0741c3e99839a1c4b0f1a942b.

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,49 @@
+import React from 'react';
+import '../../Components/Footer/Footer.css';
+import FooterLogo from '../Assets/logo.png';
+import CallIcon from '@mui/icons-material/Call';
+import SmsIcon from '@mui/icons-material/Sms';
+import EmailIcon from '@mui/icons-material/Email';
+
+
+function Footer() {
+  return (
+  <footer>
+    <div className='footer'>
+        <div className="column">
+          <img src={FooterLogo} alt="" />
+          <h2>Elysian Mind Care</h2>
+        </div>
+        <div className="column">
+          <h4>Resources</h4>
+          <ul>
+          <li>Home</li>
+          <li>About</li>
+          <li>Contact</li>
+          <li>Get Started</li>
+          </ul>
+        </div>
+        <div className="column">
+          <h4>Find Us on Social media</h4>
+          <ul>
+            <li>X</li>
+            <li>Instagram</li>
+            <li>Youtube</li>
+            <li>Linkedin</li>
+          </ul>
+        </div>
+        <div className="column">
+          <h4>Contact Us</h4>
+          <ul>
+            <li><CallIcon /><span>Call Us</span></li>
+            <li><SmsIcon /><span>Message Us</span></li>
+            <li><EmailIcon /><span>Email Us</span></li>
+          </ul>
+        </div>
+    </div>
+    <p>&copy; 2024 ElysianMind.com</p>
+  </footer>
+  )
+}
+
+export default Footer
